3526c79eed8e4bb992ab0e122436a7a796323680
John kennedy <jk_curso2018@hotmail.com>
2019-10-24 20:07:04 -0300

    24/10/2019 atualiza

---
+++ inicio.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Novo Documento</title>
+
+    <!-- <style>
+        h2{
+            color: red;
+        }
+    </style> -->
+
+</head>
+<body>
+
+    <h1>Olar mundo</h1>
+
+
+    <?php
+    
+       echo "Olar mundo!!" ;
+
+       echo "<h2>Olar mundo!!</h2>" ;
+
+       print "jk";
+    
+    ?>
+
+    <!-- <?= texto; ?> -->
+
+</body>
+</html>
