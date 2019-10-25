40c40af31b0b32173f61ac218ba7da60c1dcf2da
John kennedy <jk_curso2018@hotmail.com>
2019-10-24 21:45:35 -0300

    24/10/2019 atualizaa

---
+++ array.php
@@ -0,0 +1,28 @@
+<?php
+    $array = array(1, 2, 3, 4, 5);
+
+    print_r($array);
+    // var_dump($array);
+
+    echo "***** <br>";
+    
+    $array1[] = 6;
+
+    print_r($array1);
+
+    echo "++++++ <br>";
+
+    $primeiroquarto  = array(1 => 'Janeiro', 'Fevereiro', 'Março');
+
+    echo "<br> //////";
+    print_r($primeiroquarto);
+    echo "<br> -+-+-+";
+    count($primeiroquarto);
+    echo "<br>*-+/*-+/*-";
+    var_dump($array);
+
+
+?>  
+
+
+<!-- https://www.php.net/manual/pt_BR/language.types.array.php -->
