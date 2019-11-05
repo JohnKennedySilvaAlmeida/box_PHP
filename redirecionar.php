94f3f6a4d1c8c7cb4702bfaee0ab6f140598dec1
John kennedy <jk_curso2018@hotmail.com>
2019-11-05 20:56:23 -0300

    05/11/2019 atualiza

---
+++ redirecionar.php
@@ -0,0 +1,7 @@
+<?php
+
+    header("Location: https://www.terra.com.br");
+
+?>
+
+<!-- https://www.google.com/search?q=php+mailer&oq=php+m&aqs=chrome.2.69i57j0l2j69i60l3.6846j0j7&sourceid=chrome&ie=UTF-8 -->
