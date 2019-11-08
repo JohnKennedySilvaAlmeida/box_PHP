1abea563ba56f7f6afddcbcbbe14f853964ab666
John Doe <jk_curso2018@hotmail.com>
2019-11-08 12:34:32 -0300

    08/11/2019 atualiza

--- Index_imc.php
+++ Index_imc.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>Documento</title>
+    <title>index</title>
 
     <!-- Google Icon Font -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -20,15 +20,29 @@
 
 <body>
 
-    <h1 class="center blue">IMC</h1>
+    <nav>
+        <div class="nav-wrapper black">
+        <a href="#" class="material-icons">fitness_center</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html"></a></li>
+            <li><a href="badges.html"></a></li>
+            <li><a href="collapsible.html"></a></li>
+        </ul>
+        </div>
+    </nav><br>
+
+    <h1 class="center blue container">IMC</h1>
 
     <form action="#" method="post" class="container">
+
+        <label>Nome:</label>
+        <input type="text" name="nome" id="nome" placeholder="abcd" pattern="[a-z\s]+$" required/>
           
         <label>Altura:</label>
-        <input type="text" name="altura" id="altura" placeholder="0.00"/>
+        <input type="text" name="altura" id="altura" placeholder="0.00" pattern="[0-9.,]+$" required/>
 
         <label>Peso:</label>
-        <input type="text" name="peso" id="peso" placeholder="00" />
+        <input type="text" name="peso" id="peso" placeholder="00" pattern="[0-9.,]+$" required/>
 
         <a class="btn-floating pulse cyan accent-2"><i class="material-icons">arrow_forward</i></a>
 
@@ -38,13 +52,36 @@
 
     <?php
     
-        include("funca_imc.php");
+        include("funcaoIMC.php");
+        // apenasNum();  --- test
     
     ?>
 
-    <!-- <a class="btn-floating btn-large pulse"><input class="text-color light-blue lighten-3" type="submit"
-    value="Calcular" name="enviar" id="enviar"/>
-    <i class="material-icons">cloud</i></a> -->
+    <br>
+    <footer class="page-footer black">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <!-- <h5 class="white-text">Pratique exércícios e tenha uma boa alimentação</h5> -->
+                <p class="grey-text text-lighten-4">Vida Saúde</p>
+              </div>
+              <!-- <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div> -->
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2019 Copyright 
+            </div>
+          </div>
+        </footer>
 
 
 
--- funcaoIMC.php
+++ funcaoIMC.php
@@ -8,29 +8,30 @@
 
         if ($imc < 20) {
             // return "Abaixo do peso! <br> Seu imc é {$imc}.";
-            return "<h2 class='center red'>Abaixo do peso! <br> Seu imc é {$imc}.</h2>";
+            return "<h2 class='center red container'>Abaixo do peso! <br> Seu imc é {$imc}.</h2>";
         }
         if ($imc >= 20 && $imc <= 25) {
            // return " Peso normal! <br> Seu imc é {$imc}.";
-           return "<h2 class='center green'>Peso normal! <br> Seu imc é {$imc}.</h2>";
+           return "<h2 class='center green container'>Peso normal! <br> Seu imc é {$imc}.</h2>";
         }
         if ($imc > 25 && $imc <= 30) {
             //return "Com excesso de peso! <br> Seu imc é {$imc}.";
-            return "<h2 class='center yellow'>Com excesso de peso! <br> Seu imc é {$imc}.</h2>";
+            return "<h2 class='center yellow container'>Com excesso de peso! <br> Seu imc é {$imc}.</h2>";
         }
         if ($imc > 30 && $imc <= 35) {
            // return "Nível de obesidade! <br> Seu imc é {$imc}.";
-           return "<h2 class='center orange'>Nível de obesidade! <br> Seu imc é {$imc}.</h2>";
+           return "<h2 class='center orange container'>Nível de obesidade! <br> Seu imc é {$imc}.</h2>";
         }
         if ($imc > 35) {
             //return "Nível de obesidade mórbida! <br> Seu imc é {$imc}.";
-            return "<h2 class='center red'>Nível de obesidade mórbida! <br> Seu imc é {$imc}.</h2>";
+            return "<h2 class='center red container'>Nível de obesidade mórbida! <br> Seu imc é {$imc}.</h2>";
         }
 
     }
 
     if (isset($_POST['enviar'])) {
-            
+
+        $nome = $_POST['nome']; 
         $altura = $_POST['altura'];
         $peso = $_POST['peso'];
 
@@ -39,9 +40,20 @@
 
         $cauculo_imc = imc($peso, $altura);
 
+        echo "<h2 class='center'>Sr.{$nome}<i class='material-icons'>face</i></h2>";
         echo "<h2 class='center'>{$cauculo_imc}</h2>";
 
     }
+
+    // function apenasNum(){
+    //     $campo = $_POST['altura'];
+
+    //     if($campo != is_numeric($campo)){
+    //     echo "Não é numero";
+    //     }else{
+    //     echo "É numero";
+    //     }
+    // }
 
 ?>
 
