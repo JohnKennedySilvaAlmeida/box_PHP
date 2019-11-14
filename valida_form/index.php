1ffee745cd30bc3d249c54ba5bf0c56f88f08036
John Doe <jk_curso2018@hotmail.com>
2019-11-14 12:57:35 -0300

    14/11/2019 atualiza

--- valida_form/index.php
+++ valida_form/index.php
@@ -1,35 +1,24 @@
-<!doctype html>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		
-		<title> Formulário HTML </title>
-		
-		<style>
-		label{
-			display: block;
-		}
-		</style>
-	</head>
-	
-<body>
-		<form action="enviar.php" method="post">
-			<label for="nome">Nome:</label>
-			<input type="text" name="nome" id="nome" required> <br>
-			
-			<label for="idade">Idade:</label>
-			<input type="number" name="idade" id="idade" required> <br>
-			
-			<label for="site">Site: </label>
-			<input type="url" name="site" id="site" required> <br>
-			
-			<label for="email">Email: </label>
-			<input type="email" name="email" id="email" required> <br>
-			
-			<label for="mensagem">Mensagem: </label>
-			<textarea name="mensagem" id="mensagem" required></textarea> <br>
-			
-			<input type="submit" value="Enviar">
-		</form>
-	</body>
-</html>
+<?php
+  $login_cookie = $_COOKIE['login'];
+    if(isset($login_cookie)){
+      echo"Bem-Vindo, $login_cookie <br>";
+      echo"Essas informações <font color='red'>PODEM</font> ser acessadas por você";
+    }else{
+      echo"Bem-Vindo, convidado <br>";
+      echo"Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você";
+      echo"<br><a href='login.html'>Faça Login</a> Para ler o conteúdo";
+    }
+?>
+
+
+
+
+
+
+<!-- Create table usuarios (
+ID Int UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+login Varchar(30),
+senha Varchar(40),
+Primary Key (ID)) ENGINE = MyISAM; 
+
+banco d dados-->
